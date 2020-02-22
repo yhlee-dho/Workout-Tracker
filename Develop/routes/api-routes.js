31c85952712ee938bc610e5b0c20e07ee26e6004
unknown <yhlee89.dho@gmail.com>
2020-02-22 07:48:25 -0600

    init, api-routes

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,39 @@
+const db = requre("../models");
+
+module.exports = function(app) {
+    app.get("/api/workouts", function(req, res) {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.post("/api/workouts", function(req, res) {
+        db.Workout.create({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+    
+    app.put("/api/workouts/:id", function({ body, params }, res) {
+        db.Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true }
+        ).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts/range", function(req, res) {
+        db.Workout.find({}).limit(5).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+}
