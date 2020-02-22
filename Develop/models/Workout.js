ab966dc77274f50cbadefac0c949ef479055c1f6
unknown <yhlee89.dho@gmail.com>
2020-02-22 08:29:46 -0600

    schema

---
+++ Develop/models/Workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    exercises: [{
+        type: {
+            type: String,
+            trim: true
+        },
+        name: {
+            type: String,
+            trim: true
+        },
+        duration: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        }
+    }],
+    day: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+WorkoutSchema.virtual("totalDuration").get(function() {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.expors = Workout;
